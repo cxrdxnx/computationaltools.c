1013b68754bdf26346a1aefb68d6dd1566cc605b
cxrdxnx <mateoosoriocardonadev@gmail.com>
2025-08-31 08:54:33 -0500

    Update Standard_Deviation.cpp

--- Standard_Deviation.cpp
+++ Standard_Deviation.cpp
@@ -1,5 +1,4 @@
 
-
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -9,13 +8,9 @@ int main(void) {
     int n;
 
     while (scanf("%d", &n) == 1 && n > 0) {
-        if (n <= 1) {
-
-            printf("0.000000\n");
-            continue;
-        }
+        
             long double nn = (long double)n;
-            // desviacion estandar: sqrt(n*(n+1)/3)
+    
             long double sd = sqrtl( nn * (nn + 1.0) / 3.0);
             printf("%.6Lf\n", sd);
     }
